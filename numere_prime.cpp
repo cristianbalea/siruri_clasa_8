5551a1d0a19d98bcc258b90e913d83ea1c997ccf
Cristian Balea <127232514+cristianbalea@users.noreply.github.com>
2023-05-19 09:03:46 +0300

    Update numere_prime.cpp

--- numere_prime.cpp
+++ numere_prime.cpp
@@ -7,7 +7,7 @@ Pseudocod:
 	ok <- 1 // presupunem ca numarul e prim
 	pentru j <- 2, n/2
 		daca x % j == 0 atunci // daca are un divizor in afara de 1 si el insusi nu e prim
-			ok = 0	
+			ok <- 0	
 	daca x = 0 sau x = 1 atunci ok <- 0
 	
 	daca ok = 1 atunci prim, nr <- nr + 1
